40685a572dabf7c5c37aff76d2feaf77e07c850e
btlmo.hoangquanghuy <btlmo.hoangquanghuy@gmail.com>
2021-12-19 07:49:44 +0700

    reset UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import Feed from "./Feed";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import Widgets from "./Widgets";
+
 
 function App() {
     return (
         <div className="app">
-            {/* Header */}
-            <Header />
-            <div className="appBody">
-              <Sidebar />
-              <Feed />
-              <Widgets />
-            </div>
         </div>
     );
 }
